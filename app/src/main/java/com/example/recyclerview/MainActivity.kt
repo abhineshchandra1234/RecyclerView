545ad3c9c1a3bbdfa61ef211da11c8b2c8d875e7
abhinesh <abhinesh.c@eegrab.com>
2021-02-04 16:39:21 +0530

    added grid view

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.recyclerview
 
+import android.accounts.AuthenticatorDescription
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var titlesList = mutableListOf<String>()
+    private var descList = mutableListOf<String>()
+    private var imagesList = mutableListOf<Int>()
+    private var gridLayoutManager: GridLayoutManager?= null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        postToList()
+        gridLayoutManager = GridLayoutManager(applicationContext,3, LinearLayoutManager.VERTICAL,false)
+        rv_recyclerView?.layoutManager = gridLayoutManager
+        //rv_recyclerView.layoutManager = LinearLayoutManager(this)
+        rv_recyclerView.adapter = RecyclerAdapter(titlesList, descList, imagesList)
+    }
+    private fun addToList(title: String, description: String, image: Int) {
+        titlesList.add(title)
+        descList.add(description)
+        imagesList.add(image)
+    }
+    private fun postToList() {
+        for (i in 1..25) {
+            addToList("Title $i", "Description $i",R.mipmap.ic_launcher_round)
+        }
     }
 }
--- app/src/main/java/com/example/recyclerview/RecyclerAdapter.kt
+++ app/src/main/java/com/example/recyclerview/RecyclerAdapter.kt
@@ -1,8 +1,11 @@
 package com.example.recyclerview
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_layout.view.*
 
@@ -12,18 +15,28 @@ RecyclerView.Adapter<RecyclerAdapter.ViewHolder>(){
     inner class ViewHolder(itemView: View)  : RecyclerView.ViewHolder(itemView) {
             val itemTitle: TextView = itemView.tv_title
             val itemDetail: TextView = itemView.tv_description
+            val itemPicture: ImageView = itemView.iv_image
 
+        init {
+            itemView.setOnClickListener { v: View ->
+                 val position: Int = adapterPosition
+                Toast.makeText(itemView.context,"You clicked on item ${position + 1}",Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.item_layout,parent,false)
+        return ViewHolder(v)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return titles.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.itemTitle.text = titles[position]
+        holder.itemDetail.text = details[position]
+        holder.itemPicture.setImageResource(images[position])
     }
 }
